76c82e33b0bc97b144805a5fe734cdd3b4eec320
Ben Manning <bmanning@me.com>
2015-05-20 19:49:35 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-
-
-
-
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-
-
- 
+  root 							'static_pages#home'
+  get 'help' 		=> 	'static_pages#help'
+  get 'about' 	=> 	'static_pages#about'
+  get 'contact' => 	'static_pages#contact'
+  get 'signup'  => 	'users#new'
 end
